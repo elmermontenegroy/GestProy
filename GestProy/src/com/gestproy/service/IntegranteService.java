17a2bcb9dc0515afd58793a7052b5cd99f2dc895
Jesus <Jesus@DESKTOP-VON4LE4>
2015-12-17 11:04:31 -0500

    Avance de Mantenimiento Integrante

---
+++ GestProy/src/com/gestproy/service/IntegranteService.java
@@ -0,0 +1,34 @@
+package com.gestproy.service;
+
+import java.util.List;
+
+import com.gestproy.beans.IntegranteDTO;
+
+public class IntegranteService {
+
+	public IntegranteDTO obtenerIntegrante(int integranteId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void registrarIntegrante(IntegranteDTO integrante) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<IntegranteDTO> listarIntegrantes(IntegranteDTO integranteDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void actualizarIntegrante(IntegranteDTO integrante) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void eliminarIntegrantes(String txtEliminar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
